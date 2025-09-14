c88b853d7b5a7ce4cb7606aa6c8f195184a9a086
Will Holmes <willholmes787@gmail.com>
2025-09-13 22:35:56 -0500

    Created a private chess board variable for chessboard.java. Also implemented getpiececolor and getpiecetype with associated variables for chesspiece.java

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,9 +7,10 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private int[][] board;
 
     public ChessBoard() {
-        
+        board = new int[8][8];
     }
 
     /**
