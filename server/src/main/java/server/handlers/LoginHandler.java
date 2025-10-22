2c8cdf605dad5fd425e2eda13b71e487fb1be9dd
Will Holmes <willholmes787@gmail.com>
2025-10-22 00:28:35 -0500

    added exceptions handlers to login endpoint. login endpoint now passes all tests

--- server/src/main/java/server/handlers/LoginHandler.java
+++ server/src/main/java/server/handlers/LoginHandler.java
@@ -2,6 +2,8 @@ package server.handlers;
 
 import com.google.gson.Gson;
 import dataaccess.MemoryDatabase;
+import exception.BadRequestException;
+import exception.UnauthorizedException;
 import io.javalin.http.Context;
 import model.requests.LoginRequest;
 import server.service.LoginService;
@@ -17,9 +19,20 @@ public class LoginHandler {
     }
 
     public void handle(Context ctx) {
-        LoginRequest loginRequest = new Gson().fromJson(ctx.body(), LoginRequest.class);
-        LoginResponse loginResponse = loginService.loginUser(loginRequest);
-        ctx.status(200);
-        ctx.result(new Gson().toJson(loginResponse));
+        try {
+            LoginRequest loginRequest = new Gson().fromJson(ctx.body(), LoginRequest.class);
+            if (loginRequest.username() == null || loginRequest.password() == null) {
+                throw new BadRequestException("Error: bad request");
+            }
+            LoginResponse loginResponse = loginService.loginUser(loginRequest);
+            ctx.status(200);
+            ctx.result(new Gson().toJson(loginResponse));
+        } catch (BadRequestException e) {
+            ctx.status(e.getCode());
+            ctx.result(new Gson().toJson(e.messageWrapper()));
+        } catch (UnauthorizedException e) {
+            ctx.status(e.getCode());
+            ctx.result(new Gson().toJson(e.messageWrapper()));
+        }
     }
 }
